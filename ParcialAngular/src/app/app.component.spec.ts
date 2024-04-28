6e4e55ad8f4d0e93b5c4619aa6dfb1495ab56a6b
Aira <a.solanop@uniandes.edu.co>
2024-04-28 17:16:22 -0500

    Tests

--- ParcialAngular/src/app/app.component.spec.ts
+++ ParcialAngular/src/app/app.component.spec.ts
@@ -1,15 +1,17 @@
 import { TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
+import { PlantasListComponent } from './plantas/plantas-list/plantas-list.component';
+import { HttpClientModule } from '@angular/common/http';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [
-        RouterTestingModule
+        RouterTestingModule, HttpClientModule
       ],
       declarations: [
-        AppComponent
+        AppComponent, PlantasListComponent
       ],
     }).compileComponents();
   });
@@ -26,10 +28,4 @@ describe('AppComponent', () => {
     expect(app.title).toEqual('ParcialAngular');
   });
 
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, ParcialAngular');
-  });
 });
--- ParcialAngular/src/app/plantas/plantas-list/plantas-list.component.spec.ts
+++ ParcialAngular/src/app/plantas/plantas-list/plantas-list.component.spec.ts
@@ -4,14 +4,23 @@ import { By } from '@angular/platform-browser';
 import { DebugElement } from '@angular/core';
 
 import { PlantasListComponent } from './plantas-list.component';
+import { HttpClientModule } from '@angular/common/http';
+import { PlantaService } from '../planta.service';
+import { faker } from '@faker-js/faker';
+import { Planta } from '../planta';
+
+
 
 describe('PlantasListComponent', () => {
   let component: PlantasListComponent;
   let fixture: ComponentFixture<PlantasListComponent>;
+  let debug: DebugElement;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ PlantasListComponent ]
+      imports: [HttpClientModule],
+      declarations: [ PlantasListComponent ],
+      providers: [PlantaService]
     })
     .compileComponents();
   }));
@@ -20,9 +29,30 @@ describe('PlantasListComponent', () => {
     fixture = TestBed.createComponent(PlantasListComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
+    for (let i = 0; i<3; i++){
+      const planta = new Planta(
+        faker.number.int(),
+        faker.company.name(),
+        faker.company.name(),
+        faker.string.alphanumeric(),
+        faker.number.int(),
+        faker.string.alpha(),
+        faker.string.alpha()
+      );
+      component.plantas.push(planta)
+    }
+    fixture.detectChanges();
+    debug = fixture.debugElement;
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+  it('should have 4 <tr> rows', () => {
+    expect(debug.queryAll(By.css('tr'))).toHaveSize(4);
+  });
+
+  it('should have 12 <td> elements', () => {
+    expect(debug.queryAll(By.css('td'))).toHaveSize(12);
   });
 });
